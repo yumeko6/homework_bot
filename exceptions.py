abce02ef4203b50e31663bd5f667e67abce84e3a
Виктор Тузовский <aletissarichard@gmail.com>
2022-03-29 23:39:45 +0300

    Custom exceptions changes

--- exceptions.py
+++ exceptions.py
@@ -13,26 +13,6 @@ class ApiResponseError(Exception):
         return 'Ошибка в ответе API'
 
 
-class HomeworkTypeError(Exception):
-    def __str__(self):
-        return 'Список домашних работ не является списком'
-
-
-class HomeworkNameError(Exception):
-    def __str__(self):
-        return 'В ответе API отсутствует ключ homework_name'
-
-
 class HomeworkStatusError(Exception):
     def __str__(self):
         return 'В ответе API отсутствует ключ status'
-
-
-class ApiResponseTypeError(Exception):
-    def __str__(self):
-        return 'Ответ API не является словарем'
-
-
-class TestError(Exception):
-    def __str__(self):
-        return '123'
